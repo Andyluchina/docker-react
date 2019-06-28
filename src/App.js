9b0c0cf8522f9d9c1a017b8f7b8a0ee2ee000404
Andy <qingjielu990916@gmail.com>
2019-06-28 14:42:13 +0800

    added travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World From Andy</p>
+        <p>This is a message from Master Andy</p>
         <a
           className="App-link"
           href="https://reactjs.org"
